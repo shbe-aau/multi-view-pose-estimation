bad245ae37511094e835610ab5231e6816b518eb
Stefan Hein Bengtson <shbe@create.aau.dk>
2020-12-16 10:00:25 +0100

    small changes

--- pytorch3d/scripts/embed-all.sh
+++ pytorch3d/scripts/embed-all.sh
@@ -1,5 +1,5 @@
 # Eval object 1-20
-for OBJ_ID in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
+for OBJ_ID in 21 22 23 24 25 26 27 28 29 30 #01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
 do
     ae_embed cad_autoencoder/obj${OBJ_ID}
     wait
--- pytorch3d/scripts/eval-all.sh
+++ pytorch3d/scripts/eval-all.sh
@@ -1,6 +1,6 @@
 
 #Eval object 1-20
-for OBJ_ID in 01 02 03 04 05 06 07 08 09 10 11 12 #13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+for OBJ_ID in 21 22 23 24 25 26 27 28 29 30 #01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 do
     bash run-eval.sh ${OBJ_ID} "all-objs-99epochs" "pytorch3d/output/depth/all-objs/obj${OBJ_ID}/models/model-epoch99.pt" "train"
     wait
