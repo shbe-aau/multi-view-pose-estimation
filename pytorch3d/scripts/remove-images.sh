85d0ca242792d9d37cb75fed92e88295789b2804
Stefan <shbe@create.aau.dk>
2021-01-15 14:32:07 +0100

    updated remove images script to also remove some of the validation images

--- pytorch3d/scripts/remove-images.sh
+++ pytorch3d/scripts/remove-images.sh
@@ -4,7 +4,7 @@ find ../output/ -wholename "*/images/*.png" -delete
 # Remove all validation images except for epoch *0 and *9
 for NUM in 1 2 3 4 5 6 7 8
 do
-    find ./ -wholename "*epoch*${NUM}-*" -delete
+    find ../output/ -wholename "*/val-images/epoch*${NUM}-batch*.png" -delete
     wait
 done
 
