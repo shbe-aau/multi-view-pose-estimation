d8e5530375881cf318a6123c90c62fb9990ffb41
Stefan Hein Bengtson <shbe@create.aau.dk>
2021-01-28 16:34:33 +0100

    made the model with four extra layers and skip connections the default one!

--- pytorch3d/Model.py
+++ pytorch3d/Model.py
@@ -14,27 +14,33 @@ class Model(nn.Module):
 
         # Upscale lantent vector
         self.l01 = nn.Linear(128,128)
-        self.l02 = nn.Linear(256,256)
+        self.l02 = nn.Linear(128+128,128)
+        self.l03 = nn.Linear(128+128,128)
+        self.l04 = nn.Linear(128+128,128)
 
         self.bn01 = nn.BatchNorm1d(128)
-        self.bn02 = nn.BatchNorm1d(256)
+        self.bn02 = nn.BatchNorm1d(128)
+        self.bn03 = nn.BatchNorm1d(128)
+        self.bn04 = nn.BatchNorm1d(128)
 
         # Regress pose using the feature vector
-        self.l1 = nn.Linear(256+128,512)
-        self.l2 = nn.Linear(512,256)
-        self.l3 = nn.Linear(256,self.output_size)
+        self.l1 = nn.Linear(128+128,128)
+        self.l2 = nn.Linear(128,64)
+        self.l3 = nn.Linear(64,self.output_size)
 
-        self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.bn2 = nn.BatchNorm1d(64)
 
 
     # Input: x = lantent code
     # Output: y = pose as quaternion
     def forward(self,x0):
         x1 = F.relu(self.bn01(self.l01(x0))) #output = 128
-        x2 = F.relu(self.bn02(self.l02(torch.cat([x0, x1], dim=1)))) #output = 256
+        x2 = F.relu(self.bn02(self.l02(torch.cat([x0, x1], dim=1))))
+        x3 = F.relu(self.bn03(self.l03(torch.cat([x1, x2], dim=1))))
+        x4 = F.relu(self.bn04(self.l04(torch.cat([x2, x3], dim=1))))
 
-        x = F.relu(self.bn1(self.l1(torch.cat([x1, x2], dim=1))))
+        x = F.relu(self.bn1(self.l1(torch.cat([x3, x4], dim=1))))
         x = F.relu(self.bn2(self.l2(x)))
         y = self.l3(x)
         confs = F.softmax(y[:,:self.num_views], dim=1)
