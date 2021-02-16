c17c79878e3c2d278bee7ccb41f2db33ebf76786
Stefan Hein Bengtson <shbe@create.aau.dk>
2021-02-16 21:25:05 +0100

    fixed remove-images script to also be able to remove un-needed models

--- pytorch3d/scripts/remove-images.sh
+++ pytorch3d/scripts/remove-images.sh
@@ -11,6 +11,6 @@ done
 # # Remove all models except for epoch *0 and *9
 # for NUM in 1 2 3 4 5 6 7 8
 # do
-#     find ./ -wholename "*model-epoch*${NUM}.pt" -delete
+#     find ../output/ -wholename "*model-epoch*${NUM}.pt" -delete
 #     wait
 # done
