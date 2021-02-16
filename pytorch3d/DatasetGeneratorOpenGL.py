5b8cec31ccda36d156cb8a8fffee75d99a781c18
Stefan Hein Bengtson <shbe@create.aau.dk>
2021-02-16 19:40:51 +0100

    fixed small stuff in pose sampling with epsilon checking

--- pytorch3d/DatasetGeneratorOpenGL.py
+++ pytorch3d/DatasetGeneratorOpenGL.py
@@ -457,15 +457,17 @@ class DatasetGenerator():
         # Convert quaternion to rotation matrix
         q = np.array(random_quat, dtype=np.float64, copy=True)
         n = np.dot(q, q)
-        if n < 0.0001: #_EPS:
-            return np.identity(4)
-        q *= math.sqrt(2.0 / n)
-        q = np.outer(q, q)
-        R = np.array([
-            [1.0-q[2, 2]-q[3, 3],     q[1, 2]-q[3, 0],     q[1, 3]+q[2, 0], 0.0],
-            [    q[1, 2]+q[3, 0], 1.0-q[1, 1]-q[3, 3],     q[2, 3]-q[1, 0], 0.0],
-            [    q[1, 3]-q[2, 0],     q[2, 3]+q[1, 0], 1.0-q[1, 1]-q[2, 2], 0.0],
-            [                0.0,                 0.0,                 0.0, 1.0]])
+        
+        if n < numpy.finfo(float).eps * 4.0:
+            R = np.identity(4)
+        else:
+            q *= math.sqrt(2.0 / n)
+            q = np.outer(q, q)
+            R = np.array([
+                [1.0-q[2, 2]-q[3, 3],     q[1, 2]-q[3, 0],     q[1, 3]+q[2, 0], 0.0],
+                [    q[1, 2]+q[3, 0], 1.0-q[1, 1]-q[3, 3],     q[2, 3]-q[1, 0], 0.0],
+                [    q[1, 3]-q[2, 0],     q[2, 3]+q[1, 0], 1.0-q[1, 1]-q[2, 2], 0.0],
+                [                0.0,                 0.0,                 0.0, 1.0]])
         R = R[:3,:3]
 
         # Convert R matrix from opengl to pytorch format
