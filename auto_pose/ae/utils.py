ac9a1d42a9722a9d93ab20bf80cafc66a382faaa
Stefan Hein Bengtson <shbe@create.aau.dk>
2021-04-16 13:24:59 +0200

    updated auto_pose AE to the multipath version from the official repo

--- auto_pose/ae/utils.py
+++ auto_pose/ae/utils.py
@@ -1,8 +1,10 @@
 
 import os
+import pickle
 import numpy as np
 import functools
 import cv2
+import tensorflow as tf
 
 # https://danijar.com/structuring-your-tensorflow-models/
 def lazy_property(function):
@@ -38,12 +40,19 @@ def get_checkpoint_dir(log_dir):
     )
 
 def get_log_dir(workspace_path, experiment_name, experiment_group=''):
-    return os.path.join(
+    log_dir = os.path.join(
         workspace_path, 
         'experiments',
         experiment_group,
         experiment_name
     )
+    if not os.path.exists(log_dir):
+        log_dir = os.path.join(
+            workspace_path, 
+            experiment_group,
+            experiment_name
+        )
+    return log_dir
 
 def get_train_fig_dir(log_dir):
     return os.path.join(
@@ -57,12 +66,23 @@ def get_train_config_exp_file_path(log_dir, experiment_name):
         '{}.cfg'.format(experiment_name)
     )
 
-def get_checkpoint_basefilename(log_dir):
-    return os.path.join(
+def get_checkpoint_basefilename(log_dir, model_path=False, latest=False, joint=False):
+    import glob
+
+    file_name = os.path.join(
         log_dir,
         'checkpoints',
         'chkpt'
     )
+    if joint:
+        file_name += '-joint'
+    if model_path:
+        file_name += '-' + os.path.basename(model_path).split('.')[0]
+    if latest:
+        file_name = file_name + '-' + str(latest)
+
+    return file_name
+
 
 def get_config_file_path(workspace_path, experiment_name, experiment_group=''):
     return os.path.join(
@@ -89,6 +109,61 @@ def get_eval_dir(log_dir, evaluation_name, data):
         data
     )
 
+def create_summaries(multi_queue, decoders, ae):
+    tf.summary.histogram('mean_loss', ae._encoder.z)
+    tf.summary.scalar('total_loss', ae.loss)
+    for j,d in enumerate(decoders):
+        tf.summary.scalar('reconst_loss_%s' % j, d.reconstr_loss)
+    rand_idcs = tf.random_shuffle(tf.range(multi_queue._batch_size * multi_queue._num_objects), seed=0)
+    print(len(decoders), rand_idcs.shape[0], rand_idcs)
+    tf.summary.image('input', tf.gather(tf.concat([el[0] for el in multi_queue.next_element],0),rand_idcs), max_outputs=4)
+    tf.summary.image('reconstruction_target', tf.gather(tf.concat([el[2] for el in multi_queue.next_element],0),rand_idcs), max_outputs=4)
+    tf.summary.image('reconstruction', tf.gather(tf.concat([decoder.x for decoder in decoders], axis=0), rand_idcs), max_outputs=4)
+    return
+
+def save_pickled_data(data, file_path):
+    """Saves data to file_path. Tries to be python2/3 compatible
+    
+    Args:
+        data (any): Data to be saved
+        file_path (String): Output filepath
+    
+    Return:
+        See pickle.dump return
+    """
+    with open(file_path, "wb") as f:
+        try:
+            # Python3
+            return pickle.dump(data,
+                               f,
+                               protocol=2,
+                               fix_imports=True)
+        except TypeError:
+            # Python2
+            return pickle.dump(data,
+                               f,
+                               protocol=2)
+
+def load_pickled_data(file_path):
+    """Loads a pickled file. Tries to be python2/3 compatible
+    
+    Args:
+        file_path (string): Input filepath
+        
+    Return:
+        loaded_obj (any): Data which was loaded from file
+    """
+    with open(file_path, "rb") as f:
+        try:
+            # Python3
+            loaded_obj = pickle.load(f,
+                        fix_imports=True,
+                        encoding="bytes")
+        except TypeError:
+            # Python2
+            loaded_obj = pickle.load(f)
+    return loaded_obj
+            
 
 def tiles(batch, rows, cols, spacing_x=0, spacing_y=0, scale=1.0):
     if batch.ndim == 4:
@@ -116,3 +191,9 @@ def tiles(batch, rows, cols, spacing_x=0, spacing_y=0, scale=1.0):
                     img[start_y:end_y,start_x:end_x,0] = cv2.resize(batch[i], (W,H))
             i += 1
     return img
+
+def load_depth2(path):
+    import scipy.misc
+    d = scipy.misc.imread(path)
+    d = d.astype(np.float32)
+    return d
