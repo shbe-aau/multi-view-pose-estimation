832c64bd1b746b881df70c31091dbe8b84292f3a
Stefan Hein Bengtson <shbe@create.aau.dk>
2021-04-19 14:42:02 +0200

    updated Encoder.py to be compatible with new encoder weights trained with translation in gt (i.e. no centering)

--- pytorch3d/Encoder.py
+++ pytorch3d/Encoder.py
@@ -22,43 +22,45 @@ class Encoder(nn.Module):
         global __weights_dict
         __weights_dict = load_weights(weight_file)
 
-        self.obj1_18_v2_conv2d_Conv2D = self.__conv(2, name='obj1_18_v2/conv2d/Conv2D', in_channels=3, out_channels=128, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
-        self.obj1_18_v2_batch_normalization_FusedBatchNorm = self.__batch_normalization(2, 'obj1_18_v2/batch_normalization/FusedBatchNorm', num_features=128, eps=0.0010000000474974513, momentum=0.0)
-        self.obj1_18_v2_conv2d_1_Conv2D = self.__conv(2, name='obj1_18_v2/conv2d_1/Conv2D', in_channels=128, out_channels=256, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
-        self.obj1_18_v2_batch_normalization_1_FusedBatchNorm = self.__batch_normalization(2, 'obj1_18_v2/batch_normalization_1/FusedBatchNorm', num_features=256, eps=0.0010000000474974513, momentum=0.0)
-        self.obj1_18_v2_conv2d_2_Conv2D = self.__conv(2, name='obj1_18_v2/conv2d_2/Conv2D', in_channels=256, out_channels=512, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
-        self.obj1_18_v2_batch_normalization_2_FusedBatchNorm = self.__batch_normalization(2, 'obj1_18_v2/batch_normalization_2/FusedBatchNorm', num_features=512, eps=0.0010000000474974513, momentum=0.0)
-        self.obj1_18_v2_conv2d_3_Conv2D = self.__conv(2, name='obj1_18_v2/conv2d_3/Conv2D', in_channels=512, out_channels=512, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
-        self.obj1_18_v2_batch_normalization_3_FusedBatchNorm = self.__batch_normalization(2, 'obj1_18_v2/batch_normalization_3/FusedBatchNorm', num_features=512, eps=0.0010000000474974513, momentum=0.0)
-        self.obj1_18_v2_dense_MatMul = self.__dense(name = 'obj1_18_v2/dense/MatMul', in_features = 32768, out_features = 128, bias = True)
+        base_name = (list(__weights_dict.keys())[0].split('/'))[0]
+
+        self.encoder_conv2d_Conv2D = self.__conv(2, name=base_name+'/conv2d/Conv2D', in_channels=3, out_channels=128, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
+        self.encoder_batch_normalization_FusedBatchNorm = self.__batch_normalization(2, base_name+'/batch_normalization/FusedBatchNorm', num_features=128, eps=0.0010000000474974513, momentum=0.0)
+        self.encoder_conv2d_1_Conv2D = self.__conv(2, name=base_name+'/conv2d_1/Conv2D', in_channels=128, out_channels=256, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
+        self.encoder_batch_normalization_1_FusedBatchNorm = self.__batch_normalization(2, base_name+'/batch_normalization_1/FusedBatchNorm', num_features=256, eps=0.0010000000474974513, momentum=0.0)
+        self.encoder_conv2d_2_Conv2D = self.__conv(2, name=base_name+'/conv2d_2/Conv2D', in_channels=256, out_channels=512, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
+        self.encoder_batch_normalization_2_FusedBatchNorm = self.__batch_normalization(2, base_name+'/batch_normalization_2/FusedBatchNorm', num_features=512, eps=0.0010000000474974513, momentum=0.0)
+        self.encoder_conv2d_3_Conv2D = self.__conv(2, name=base_name+'/conv2d_3/Conv2D', in_channels=512, out_channels=512, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
+        self.encoder_batch_normalization_3_FusedBatchNorm = self.__batch_normalization(2, base_name+'/batch_normalization_3/FusedBatchNorm', num_features=512, eps=0.0010000000474974513, momentum=0.0)
+        self.encoder_dense_MatMul = self.__dense(name = base_name+'/dense/MatMul', in_features = 32768, out_features = 128, bias = True)
 
     def forward(self, x):
-        obj1_18_v2_Flatten_flatten_Reshape_shape_1 = torch.tensor(-1, dtype=torch.int32)
-        obj1_18_v2_conv2d_Conv2D_pad = F.pad(x, (1, 2, 1, 2))
-        obj1_18_v2_conv2d_Conv2D = self.obj1_18_v2_conv2d_Conv2D(obj1_18_v2_conv2d_Conv2D_pad)
-        obj1_18_v2_conv2d_Relu = F.relu(obj1_18_v2_conv2d_Conv2D)
-        obj1_18_v2_batch_normalization_FusedBatchNorm = self.obj1_18_v2_batch_normalization_FusedBatchNorm(obj1_18_v2_conv2d_Relu)
-        obj1_18_v2_conv2d_1_Conv2D_pad = F.pad(obj1_18_v2_batch_normalization_FusedBatchNorm, (1, 2, 1, 2))
-        obj1_18_v2_conv2d_1_Conv2D = self.obj1_18_v2_conv2d_1_Conv2D(obj1_18_v2_conv2d_1_Conv2D_pad)
-        obj1_18_v2_conv2d_1_Relu = F.relu(obj1_18_v2_conv2d_1_Conv2D)
-        obj1_18_v2_batch_normalization_1_FusedBatchNorm = self.obj1_18_v2_batch_normalization_1_FusedBatchNorm(obj1_18_v2_conv2d_1_Relu)
-        obj1_18_v2_conv2d_2_Conv2D_pad = F.pad(obj1_18_v2_batch_normalization_1_FusedBatchNorm, (1, 2, 1, 2))
-        obj1_18_v2_conv2d_2_Conv2D = self.obj1_18_v2_conv2d_2_Conv2D(obj1_18_v2_conv2d_2_Conv2D_pad)
-        obj1_18_v2_conv2d_2_Relu = F.relu(obj1_18_v2_conv2d_2_Conv2D)
-        obj1_18_v2_batch_normalization_2_FusedBatchNorm = self.obj1_18_v2_batch_normalization_2_FusedBatchNorm(obj1_18_v2_conv2d_2_Relu)
-        obj1_18_v2_conv2d_3_Conv2D_pad = F.pad(obj1_18_v2_batch_normalization_2_FusedBatchNorm, (1, 2, 1, 2))
-        obj1_18_v2_conv2d_3_Conv2D = self.obj1_18_v2_conv2d_3_Conv2D(obj1_18_v2_conv2d_3_Conv2D_pad)
-        obj1_18_v2_conv2d_3_Relu = F.relu(obj1_18_v2_conv2d_3_Conv2D)
-        obj1_18_v2_batch_normalization_3_FusedBatchNorm = self.obj1_18_v2_batch_normalization_3_FusedBatchNorm(obj1_18_v2_conv2d_3_Relu)
-        obj1_18_v2_Flatten_flatten_Shape = list(obj1_18_v2_batch_normalization_3_FusedBatchNorm.size())
-        #obj1_18_v2_Flatten_flatten_Reshape = torch.reshape(input = obj1_18_v2_batch_normalization_3_FusedBatchNorm, shape = (-1,32768))
-        intermediate = obj1_18_v2_batch_normalization_3_FusedBatchNorm.permute(0,2,3,1)
-        obj1_18_v2_Flatten_flatten_Reshape = torch.reshape(input = intermediate, shape = (-1,32768))
+        encoder_Flatten_flatten_Reshape_shape_1 = torch.tensor(-1, dtype=torch.int32)
+        encoder_conv2d_Conv2D_pad = F.pad(x, (1, 2, 1, 2))
+        encoder_conv2d_Conv2D = self.encoder_conv2d_Conv2D(encoder_conv2d_Conv2D_pad)
+        encoder_conv2d_Relu = F.relu(encoder_conv2d_Conv2D)
+        encoder_batch_normalization_FusedBatchNorm = self.encoder_batch_normalization_FusedBatchNorm(encoder_conv2d_Relu)
+        encoder_conv2d_1_Conv2D_pad = F.pad(encoder_batch_normalization_FusedBatchNorm, (1, 2, 1, 2))
+        encoder_conv2d_1_Conv2D = self.encoder_conv2d_1_Conv2D(encoder_conv2d_1_Conv2D_pad)
+        encoder_conv2d_1_Relu = F.relu(encoder_conv2d_1_Conv2D)
+        encoder_batch_normalization_1_FusedBatchNorm = self.encoder_batch_normalization_1_FusedBatchNorm(encoder_conv2d_1_Relu)
+        encoder_conv2d_2_Conv2D_pad = F.pad(encoder_batch_normalization_1_FusedBatchNorm, (1, 2, 1, 2))
+        encoder_conv2d_2_Conv2D = self.encoder_conv2d_2_Conv2D(encoder_conv2d_2_Conv2D_pad)
+        encoder_conv2d_2_Relu = F.relu(encoder_conv2d_2_Conv2D)
+        encoder_batch_normalization_2_FusedBatchNorm = self.encoder_batch_normalization_2_FusedBatchNorm(encoder_conv2d_2_Relu)
+        encoder_conv2d_3_Conv2D_pad = F.pad(encoder_batch_normalization_2_FusedBatchNorm, (1, 2, 1, 2))
+        encoder_conv2d_3_Conv2D = self.encoder_conv2d_3_Conv2D(encoder_conv2d_3_Conv2D_pad)
+        encoder_conv2d_3_Relu = F.relu(encoder_conv2d_3_Conv2D)
+        encoder_batch_normalization_3_FusedBatchNorm = self.encoder_batch_normalization_3_FusedBatchNorm(encoder_conv2d_3_Relu)
+        encoder_Flatten_flatten_Shape = list(encoder_batch_normalization_3_FusedBatchNorm.size())
+        #encoder_Flatten_flatten_Reshape = torch.reshape(input = encoder_batch_normalization_3_FusedBatchNorm, shape = (-1,32768))
+        intermediate = encoder_batch_normalization_3_FusedBatchNorm.permute(0,2,3,1)
+        encoder_Flatten_flatten_Reshape = torch.reshape(input = intermediate, shape = (-1,32768))
 
-        obj1_18_v2_Flatten_flatten_strided_slice = obj1_18_v2_Flatten_flatten_Shape[0:1]
-        obj1_18_v2_dense_MatMul = self.obj1_18_v2_dense_MatMul(obj1_18_v2_Flatten_flatten_Reshape)
-        obj1_18_v2_Flatten_flatten_Reshape_shape = [obj1_18_v2_Flatten_flatten_strided_slice,obj1_18_v2_Flatten_flatten_Reshape_shape_1]
-        return obj1_18_v2_dense_MatMul
+        encoder_Flatten_flatten_strided_slice = encoder_Flatten_flatten_Shape[0:1]
+        encoder_dense_MatMul = self.encoder_dense_MatMul(encoder_Flatten_flatten_Reshape)
+        encoder_Flatten_flatten_Reshape_shape = [encoder_Flatten_flatten_strided_slice,encoder_Flatten_flatten_Reshape_shape_1]
+        return encoder_dense_MatMul
 
 
     @staticmethod
